d99cb2c87cdb5527b54a9e67c7fee1b5ec278b90
Anton Pomozov <pomozoff@gmail.com>
2016-06-04 02:26:44 +0300

    add: Base table view controller

---
+++ SwiftChat/ChatTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ChatTableViewController.swift
+//  SwiftChat
+//
+//  Created by Антон on 03.06.16.
+//  Copyright © 2016 Akademon Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ChatTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
