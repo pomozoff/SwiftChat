8f288ab95930c40ac5cbce2857dee38907dc3493
Anton Pomozov <pomozoff@gmail.com>
2016-06-04 14:08:37 +0300

    add: Data source property

--- SwiftChat/ChatTableViewController.swift
+++ SwiftChat/ChatTableViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 
 class ChatTableViewController: UITableViewController {
+    
+    // MARK: - Properties
 
+    var dataSource: DataSource?
+    
+    // MARK: - Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ SwiftChat/Event.swift
@@ -0,0 +1,9 @@
+//
+//  Event.swift
+//  SwiftChat
+//
+//  Created by Антон on 04.06.16.
+//  Copyright © 2016 Akademon Ltd. All rights reserved.
+//
+
+import Foundation
